3c9baf98813aa69d3a6c7b0ea00bdb8136f64ed6
franzholz <franzholz@6a152407-d83c-435e-9478-febfe02dd9d6>
2015-08-06 17:52:22 +0000

    add static_info_tables_it which runs under TYPO3 6.2 and 7.0

---
+++ ext_emconf.php
@@ -0,0 +1,42 @@
+<?php
+########################################################################
+# Extension Manager/Repository config file for ext: "static_info_tables_it"
+#
+# Auto generated 20-07-2005 12:06
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write
+########################################################################
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'Static Info Tables (it)',
+	'description' => 'Italian (it) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '6.2.1',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Franz Holzinger',
+	'author_email' => 'franz@ttproducts.de',
+	'author_company' => 'jambage.com',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array (
+		'depends' => array(
+			'typo3' => '6.2.0-7.0.99',
+			'static_info_tables' => '6.2.1-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
+?>
---
+++ ext_tables.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE static_countries (
+  cn_short_it varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_currencies (
+  cu_name_it varchar(50) DEFAULT '' NOT NULL,
+  cu_sub_name_it varchar(20) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_languages (
+  lg_name_it varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_territories (
+  tr_name_it varchar(50) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE static_country_zones (
+  zn_name_it varchar(50) DEFAULT '' NOT NULL
+);
+
