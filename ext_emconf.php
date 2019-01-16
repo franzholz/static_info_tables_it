a086420ca790e641c9415ecdb1cd3c863c389f21
André Schließer <a.schliesser@zeroseven.de>
2019-01-16 08:44:52 +0100

    Prepare release version 6.7.0 for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'description' => 'Italian (it) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '6.4.5',
+	'version' => '6.7.0',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
@@ -22,8 +22,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'constraints' => array (
 		'depends' => array(
-            'typo3' => '7.6.0-8.99.99',
-            'static_info_tables' => '6.2.1-6.99.99',
+            'typo3' => '8.7.0-9.5.99',
+            'static_info_tables' => '6.7.3-6.7.99',
 		),
 		'conflicts' => array(
 		),
