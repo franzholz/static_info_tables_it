fe79f1ac0795d12ce517a81289008e293bb61f08
Franz Holzinger <franz.holzinger@festwein.de>
2024-10-29 18:20:41 +0100

    works under TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Static Info Tables (it)',
     'description' => 'Italian (it) language pack for the Static Info Tables providing localized names for countries, currencies and so on.',
     'category' => 'misc',
-    'version' => '11.5.2',
+    'version' => '11.5.3',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -13,9 +13,9 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'static_info_tables' => '11.5.0-12.4.99',
-            'addons_em' => '0.8.2-0.12.99',
+            'typo3' => '11.5.0-13.4.99',
+            'static_info_tables' => '11.5.0-13.4.99',
+            'addons_em' => '0.9.0-0.12.99',
         ],
         'conflicts' => [
         ],
